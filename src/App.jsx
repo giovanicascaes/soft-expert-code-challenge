def3ad54537703243bafe8d01841cf80d79ce42c
Giovani Cascaes <giovanicascaes@gmail.com>
2020-10-23 07:47:30 -0300

    Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "react-hot-loader";
+import React from "react";
+import { hot } from "react-hot-loader/root";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello, World!!!! </h1>
+    </div>
+  );
+}
+
+export default hot(App);
