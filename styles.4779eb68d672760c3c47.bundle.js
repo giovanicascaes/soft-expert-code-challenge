f097313035fc8847a4785e76f974582565af0dc4
Giovani Cascaes <giovanicascaes@gmail.com>
2020-10-28 03:59:48 -0300

    Updates

---
+++ styles.4779eb68d672760c3c47.bundle.js
@@ -0,0 +1,2 @@
+(self.webpackChunksoft_expert_code_challenge=self.webpackChunksoft_expert_code_challenge||[]).push([[532],{3217:(e,p,c)=>{"use strict";c.d(p,{Z:()=>l});const l={"flip-wrapper":"_2i68TNQwL1IkawWGhXXNwv",flipWrapper:"_2i68TNQwL1IkawWGhXXNwv",flip:"YJ5Te4eXWgv6rvs-WZCnq",flipper:"j9F9lNFWYbzueQQsTTi2b",front:"VlZbPwzSy-OhCzekec2oB",back:"_2DFW8h9wurPWOcgcRqsboY"}}}]);
+//# sourceMappingURL=styles.4779eb68d672760c3c47.bundle.js.map
